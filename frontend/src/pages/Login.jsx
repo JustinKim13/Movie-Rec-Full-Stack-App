0e54e4cd4fefcb5dc384190d644e61642f05cdd4
Your Name <jtkimmn13@gmail.com>
2024-06-26 21:37:17 -0500

    created navigation buttons between register and login pages

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,15 @@
-import Form from "../components/Form"
+import Form from "../components/Form";
+import { Link } from "react-router-dom";
 
 function Login() {
-    return <Form route="/api/token/" method="login" />
+    return (
+        <div>
+            <Form route="/api/token/" method="login" />
+            <div className="link-container">
+                <p>Don't have an account? <Link to="/register">Register here</Link></p>
+            </div>
+        </div>
+    );
 }
 
-export default Login
+export default Login;
